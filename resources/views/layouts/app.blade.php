55723e6fa4a11f0e94166d28691e4ab7ccfd692f
Pintiliy <mrpintiliy@gmail.com>
2016-08-23 22:49:29 +0300

    Add header and menus, slightly style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,82 +1,117 @@
 <!DOCTYPE html>
-<html lang="en">
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+	<meta charset="utf-8" />
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+	<title>Заголовок</title>
+	<meta content="" name="description" />
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon" />
 
-    <style>
-        body {
-            font-family: 'Lato';
-        }
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('/fonts/font-awesome-4.6.3/css/font-awesome.min.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('/libs/bootstrap/css/bootstrap.min.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('/libs/animate/animate.css') }}" />
 
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
+  <link rel="stylesheet" type="text/css" href="{{ asset('/css/main.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('/css/media.css') }}" />
 </head>
-<body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+<body>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
-                </a>
+	<div class="container-fluid ">
+		<nav class="navbar navbar-default">
+			<div class="container-fluid width-container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="logo" href="{{url('/')}}"><img src="{{ asset('/images/sysimg/logo.png') }}" alt="Лого сайту ALCOPROGER" /></a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+              <!-- Authentication Links -->
+              @if (Auth::guest())
+                  <li><a href="{{ url('/login') }}">Login</a></li>
+                  <li><a href="{{ url('/register') }}">Register</a></li>
+              @else
+                  <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                          {{ Auth::user()->name }} <span class="caret"></span>
+                      </a>
+
+                      <ul class="dropdown-menu" role="menu">
+                          <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                      </ul>
+                  </li>
+              @endif
+          </ul>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+    <nav class="navbar navbar-top-menu">
+        <div class='width-container'>
+          <div class=" col-xs-8 top-menu">
+            <ul>
+              <li><a href="#">Головна</a></li>
+              <li><a href="#">Блоги</a></li>
+              <li><a href="#">Люди</a></li>
+              <li><a href="#">Проекти</a></li>
+              <li><a href="#">Замовлення</a></li>
+              <li><a href="#">Про проект</a></li>
+            </ul>
+          </div>
+          <div class=" col-xs-4 search-form">
+            <div>
+              <input type="search" name="name" placeholder="Search...">
+              <button type="button" class="button-search"><i class="fa fa-search" aria-hidden="true"></i></button>
             </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
+          </div>
         </div>
     </nav>
+		<div class="row width-container">
+        @yield('content')
+		</div>
+			<div class="col-xs-3 sidebar">
+					<div>
+							<input type="text" class="form-control" placeholder="Search">
+							<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span></button>
+					</div>
+			</div>
+	</div>
 
-    @yield('content')
+	<div class="hidden"></div>
 
-    <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+	<!--[if lt IE 9]>
+	<script src="/libs/html5shiv/es5-shim.min.js"></script>
+	<script src="/libs/html5shiv/html5shiv.min.js"></script>
+	<script src="/libs/html5shiv/html5shiv-printshiv.min.js"></script>
+	<script src="/libs/respond/respond.min.js"></script>
+	<![endif]-->
+
+	<script src="{{ asset('/libs/jquery/jquery-1.11.2.min.js') }}"></script>
+	<script src="{{ asset('/libs/modernizr/modernizr.js') }}"></script>
+	<script src="{{ asset('/libs/bootstrap/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('/libs/waypoints/waypoints.min.js') }}"></script>
+	<script src="{{ asset('/libs/plugins-scroll/plugins-scroll.js') }}"></script>
+	<script src="{{ asset('/libs/animate/animate-css.js') }}"></script>
+
+	<script src="{{ asset('/libs/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+	<script src="{{ asset('/libs/imagefill/jquery-imagefill.js') }}"></script>
+	<script src="{{ asset('/libs/masonry/masonry.pkgd.min.js') }}"></script>
+
+  <script src="{{ asset('/js/common.js') }}"></script>
 </body>
 </html>
